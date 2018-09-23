8141213bd4c29a2e7859ba0673f1ad4f53f9a6f0
VHeroine <vheroinev@mail.ru>
2018-09-23 14:55:24 +0300

    Fibonacci sequence

--- Algorithms/script.js
+++ Algorithms/script.js
@@ -8,3 +8,15 @@ function append_value(ap_value, to_add) {
 document.getElementById(to_add).value += ap_value;
 return null;
 }
+
+function fib_seq(num) {
+	if (num == 0) return 0;
+	var a = 1, b = 1, i = 1, sum = 0;
+	while (i < num) {
+		sum = a + b;
+		a = b;
+		b = sum;
+		i++;
+	}
+	return a;
+}
