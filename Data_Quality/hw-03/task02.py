51152053f50becdc299b08a28294c93bf80219b5
VHeroinev <VHeroineV@mail.ru>
2021-03-09 18:12:38 +0300

    Fixed the reversing.

--- Data_Quality/hw-03/task02.py
+++ Data_Quality/hw-03/task02.py
@@ -1,16 +1,23 @@
+import os
+
 def rev_str(pr_str):
-    return ''.join(reversed(pr_str))
+    return pr_str[::-1]
 
-res_str = ''
-with open("input.txt", 'r', encoding='UTF-8') as init_file:
-    for line in init_file:
-        s = []
-        result = line.rstrip('\n\r')
-        res = result.split(' ')
-        for key in res:
-            s.append(rev_str(key))
-        str1 = ' '.join(s)
-        res_str = res_str + str1 + '\n'
+def main():
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    res_str = ''
+    with open("input.txt", 'r', encoding='UTF-8') as init_file:
+        for line in init_file:
+            s = []
+            result = line.rstrip('\n\r')
+            res = result.split(' ')
+            for key in res:
+                s.append(rev_str(key))
+            str1 = ' '.join(s)
+            res_str = res_str + str1 + '\n'
 
-with open("input.txt", 'w', encoding='UTF-8') as result_file:
-    result_file.write(res_str)
+    with open("input.txt", 'w', encoding='UTF-8') as result_file:
+        result_file.write(res_str)
+
+if __name__ == '__main__':
+    main()
